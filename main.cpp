38dca14150e944025ed9be9ea4a12e79c7476d3c
DoomKitty87 <doomkitty887@gmail.com>
2023-05-26 16:00:46 -0700

    added line rendering for wireframing

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ class WorldSpaceObject
   public:
     Vector3 position;
     vector<Vector3> points;
+    vector<int> draw_order;
 };
 
 class Space
@@ -93,12 +94,21 @@ WorldSpaceObject generateCube(Vector3 position, float vertexdistance) {
   obj.points.push_back(Vector3(-vertexdistance, vertexdistance, vertexdistance));
   obj.points.push_back(Vector3(vertexdistance, vertexdistance, vertexdistance));
 
+  obj.draw_order.push_back(0);
+  obj.draw_order.push_back(1);
+  obj.draw_order.push_back(2);
+  obj.draw_order.push_back(3);
+  obj.draw_order.push_back(4);
+  obj.draw_order.push_back(5);
+  obj.draw_order.push_back(6);
+  obj.draw_order.push_back(7);
+
   obj.position = position;
   return obj;
 }
 
 //Y axis is forward for cameras- and view is projected along it.
-vector<vector<bool> > render_camera(Camera camera, Space space) {
+pair<vector<vector<bool> >, vector<vector<bool> > > render_camera(Camera camera, Space space) {
   vector<vector<bool> > renderedPixels = vector<vector<bool> >();
   for (int h = 0; h < camera.resolution.second; h++) {
     vector<bool> currentLayer = vector<bool>();
@@ -115,8 +125,38 @@ vector<vector<bool> > render_camera(Camera camera, Space space) {
     }
     renderedPixels.push_back(currentLayer);
   }
+  //Render points
 
-  return renderedPixels;
+  vector<vector<bool> > renderedLines = vector<vector<bool> >();
+
+  for (int h = 0; h < camera.resolution.second; h++) {
+    vector<bool> currentLayer = vector<bool>();
+    currentLayer.resize(camera.resolution.first);
+    for (int w = 0; w < camera.resolution.first; w++) {
+      currentLayer[w] = false;
+      for (int i = 0; i < space.objects.size(); i++) {
+        for (int j = 1; j < space.objects[i].draw_order.size(); j++) {
+          float x0 = space.objects[i].points[space.objects[i].draw_order[j - 1]].x;
+          float x1 = space.objects[i].points[space.objects[i].draw_order[j]].x;
+          float z0 = space.objects[i].points[space.objects[i].draw_order[j - 1]].z;
+          float z1 = space.objects[i].points[space.objects[i].draw_order[j]].z;
+          for (float k = 0; k <= 1; k += 0.1) {
+            int xcheck = round(x0 * (1.0 - k) + (x1 * k));
+            int zcheck = round(z0 * (1.0 - k) + (z1 * k));
+            if (xcheck == camera.position.x + (w - camera.resolution.first / 2) && zcheck == camera.position.z + (h - camera.resolution.second / 2)) {
+              currentLayer[w] = true;
+            }
+          }
+        }
+      }
+    }
+    renderedLines.push_back(currentLayer);
+  }
+  //Render lines
+  pair<vector<vector<bool> >, vector<vector<bool> > > returnval = pair<vector<vector<bool> >, vector<vector<bool> > >();
+  returnval.first = renderedPixels;
+  returnval.second = renderedLines;
+  return returnval;
 }
 
 int main() {
@@ -143,10 +183,11 @@ int main() {
     //newpoint.y = (rand() % (cam.resolution.first) - (cam.resolution.first / 2));
     //newpoint.z = (rand() % (cam.resolution.first) - (cam.resolution.first / 2));
     //space.objects[0].points.push_back(newpoint);
-    vector<vector<bool> > pixels = render_camera(cam, space);
+    pair<vector<vector<bool> >, vector<vector<bool> > > pixels = render_camera(cam, space);
     for (int h = cam.resolution.second; h > 0; h--) {
       for (int w = 0; w < cam.resolution.first; w++) {
-        if (pixels[h - 1][w]) cout << "*";
+        if (pixels.first[h - 1][w]) cout << "*";
+        else if (pixels.second[h - 1][w]) cout << "/";
         else cout << " ";
       }
       cout << endl;
